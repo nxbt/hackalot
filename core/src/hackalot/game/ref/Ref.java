40a55a3d5c4edd09113fb0705b4d78759859862a
Brendan Thompson <nxbt1998@gmail.com>
2020-03-30 19:48:14 -0700

    Started breaking up Ref into multiple files

--- core/src/hackalot/game/ref/Ref.java
+++ core/src/hackalot/game/ref/Ref.java
@@ -15,13 +15,6 @@ public class Ref {
 		sc = new StateController();
 	}
 
-	public class State {
 
-		public final hackalot.game.state.State default_state;
-
-		public State() {
-			default_state = new hackalot.game.state.MenuState();
-		}
-	}
 
 }
